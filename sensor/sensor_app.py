206213054fd7717db2de5d93290a44fc1295e798
dvncrtraz <72676881+dvncrtr@users.noreply.github.com>
2022-06-04 16:15:10 -0700

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,12 +6,11 @@ from load_data import load_sensor_data
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
-data = load_sensor_data()
-print("Loaded records: {}".format(len(data)))
 ##############################
 
 # Module 1 code here:
-
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
